d014e742d8a7745509bde9d9feecba18f2168e56
katao <k.katasan@gmail.com>
2025-05-25 10:14:36 +0900

    htmlをサブディレクトリに移動

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 target="otoge_input_viewer"
 pyin=/mnt/c/*/Python310/Scripts/pyinstaller.exe
 $pyin $target.pyw --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" 
-cp *.html $target/
+cp -a html $target/
 cp dist/*.exe to_bin/
-cp *.html to_bin/
+cp -a html to_bin/
 cp dist/*.exe $target/
 cp version.txt $target/
 #zip $target.zip $target/* $target/*/* $target/*/*/*
