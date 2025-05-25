56cf15d0073df9ce48d056a6d6cd6d332310fa5d
katao <k.katasan@gmail.com>
2025-05-25 15:27:05 +0900

    [mod]2階層分追加するように修正

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 target="otoge_input_viewer"
 pyin=/mnt/c/*/Python310/Scripts/pyinstaller.exe
 $pyin $target.pyw --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" 
-cp -a html $target/
+cp -a html/ $target/
 cp dist/*.exe to_bin/
-cp -a html to_bin/
+cp -a html/ to_bin/
 cp dist/*.exe $target/
 cp version.txt $target/
 #zip $target.zip $target/* $target/*/* $target/*/*/*
 rm -rf $target.zip
-#zip $target.zip $target/* $target/*/* 
-zip $target.zip $target/* 
+zip $target.zip $target/*  $target/*/*
