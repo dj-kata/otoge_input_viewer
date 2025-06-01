f823acf01cf797ee2e8e66afcc6c9e2ed263d0f5
katao <k.katasan@gmail.com>
2025-06-02 00:56:31 +0900

    [mod]ビルドスクリプトをしっかり書き直した

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 target="otoge_input_viewer"
+rm -rf $target
+mkdir $target
+mkdir -p $target
 pyin=/mnt/c/*/Python310/Scripts/pyinstaller.exe
 $pyin $target.pyw --clean --noconsole --onefile --icon=icon.ico --add-data "icon.ico;./" 
+cp *.ico $target
 cp -a html/ $target/
 cp dist/*.exe to_bin/
 cp -a html/ to_bin/
